82a91bb4e7c5ab0fb9ce2c27062bf97dbd92731b
UncrownedKing1 <108270526+UncrownedKing1@users.noreply.github.com>
2023-09-30 17:36:16 +0200

    Added test connection script 'py_script.py', requirements.txt and modified .gitignore and README.md

---
+++ py_script.py
@@ -0,0 +1,23 @@
+import mysql.connector as sqlcon
+
+try:
+    connection = sqlcon.connect(host='localhost', 
+                                database='testdb', 
+                                user='testuser', 
+                                password='password')
+
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except sqlcon.Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
