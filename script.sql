7fe0ab5573389489a520567eca0aa15084bd7a6f
Jon Lumi <jlumi@constructor.university>
2023-09-30 16:36:52 +0200

    Added table 'club_participants'.

--- script.sql
+++ script.sql
@@ -10,3 +10,8 @@ CREATE TABLE clubs (
     club_name VARCHAR(255),
     club_desc VARCHAR(255)
 );
+
+CREATE TABLE club_participants (
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (club_id) REFERENCES clubs(club_id)
+);
