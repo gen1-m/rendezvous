df8d2cafa27eaa9f10532b346b4e1abe8eef78ab
UncrownedKing1 <108270526+UncrownedKing1@users.noreply.github.com>
2023-10-09 20:46:37 +0200

    Added queries.sql with queries to be used in the future.

--- queries.sql
+++ queries.sql
@@ -1,16 +1,5 @@
--- 3*N queries, 14 each member, at least N joins, at least one aggregation function, and one group by/having clause
---if you wnat to test the queries please comment out the table output
-
-
+-- Xhesi's scripts
 SELECT COUNT(*) FROM users;
-
-+----------+
-| COUNT(*) |
-+----------+
-|       13 |
-+----------+
-1 row in set (0.001 sec)
-
 
 SELECT club_name
 FROM clubs c1
@@ -21,49 +10,11 @@ WHERE EXISTS (
       AND c1.club_size = c2.club_size
 );
 
-+------------+
-| club_name  |
-+------------+
-| 'swimming' |
-| volleyball |
-+------------+
-
-
 SELECT users.username, users.user_id
 FROM users
 INNER JOIN club_participants ON users.user_id = club_participants.user_id;
 
-+------------+---------+
-| username   | user_id |
-+------------+---------+
-| xhesilda   |       1 |
-|  'alice'   |       3 |
-|  'jane'    |       4 |
-|  'john'    |       2 |
-|  'emily'   |       8 |
-|  'sarah'   |       6 |
-|  'david'   |       7 |
-|  'samuel'  |       9 |
-|  'olivia'  |      10 |
-|  'michael' |       5 |
-+------------+---------+
-
 SELECT user_id from club_participants ORDER BY join_date DESC;
-+---------+
-| user_id |
-+---------+
-|       1 |
-|       9 |
-|       8 |
-|       7 |
-|       6 |
-|       5 |
-|       4 |
-|       3 |
-|       2 |
-|      10 |
-+---------+
-
 
 SELECT clubs.club_name, COUNT(users.user_id) AS member_count
 FROM users
@@ -71,12 +22,67 @@ JOIN club_participants ON users.user_id = club_participants.user_id
 JOIN clubs ON clubs.club_id = club_participants.club_id
 GROUP BY clubs.club_name;
 
-+-----------+--------------+
-| club_name | member_count |
-+-----------+--------------+
-| 'API'     |            2 |
-| 'FINTECH' |            1 |
-| 'IBM'     |            2 |
-| 'kikbox'  |            2 |
-| GDSC      |            3 |
-+-----------+--------------+
+-- mine and Joni's scripts
+
+-- Select the usernames of club leaders:
+SELECT u.username
+FROM users u
+JOIN club_leaders cl ON u.user_id = cl.user_id;
+
+-- Select the total number of members in each club:
+SELECT c.club_name, COUNT(cm.user_id) AS member_count
+FROM clubs c
+LEFT JOIN club_members cm ON c.club_id = cm.club_id
+GROUP BY c.club_name;
+
+-- Select events with their attendees' usernames:
+SELECT e.event_name, u.username
+FROM events e
+JOIN event_attendees ea ON e.event_id = ea.event_id
+JOIN users u ON ea.user_id = u.user_id;
+
+-- Select users who are leaders of more than one club:
+SELECT u.username, COUNT(cl.club_id) AS club_count
+FROM users u
+JOIN club_leaders cl ON u.user_id = cl.user_id
+GROUP BY u.username
+HAVING club_count > 1;
+
+-- Select clubs and their event counts:
+SELECT c.club_name, COUNT(ce.event_id) AS event_count
+FROM clubs c
+LEFT JOIN club_events ce ON c.club_id = ce.club_id
+GROUP BY c.club_name;
+
+-- Select events with more than 100 attendees:
+SELECT e.event_name
+FROM events e
+JOIN event_attendees ea ON e.event_id = ea.event_id
+GROUP BY e.event_name
+HAVING COUNT(ea.user_id) > 100;Select the average club size:
+
+-- Select users who have created personal events:
+SELECT u.username, COUNT(pe.event_id) AS personal_event_count
+FROM users u
+LEFT JOIN personal_events pe ON u.user_id = pe.user_id
+GROUP BY u.username;
+
+-- Select the average club size:
+SELECT AVG(club_size) AS avg_club_size
+FROM clubs;
+
+-- Select event leaders and their event names:
+SELECT u.username, e.event_name
+FROM users u
+JOIN event_leaders el ON u.user_id = el.user_id
+JOIN events e ON el.event_id = e.event_id;
+
+-- Select users who have accepted event invitations:
+SELECT u.username
+FROM users u
+JOIN event_invitations ei ON u.user_id = ei.receiver_id
+WHERE ei.accept_before < NOW();
+
+--  Anja's scripts
+SELECT asgje FROM Anja;
+
